095d3cd7b041b8a0a080e48d9be4696674111cb5
LucieneFagundes <lfnascimento99@gmail.com>
2022-09-23 15:00:45 -0300

    refact button

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -84,7 +84,7 @@ const Login: NextPage = () => {
                     className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
                   >
                     <span className="absolute left-0 inset-y-0 flex items-center pl-3">
-                      <LockClosedIcon className="h-5 w-5 text-green-500 group-hover:text-green-400" aria-hidden="true" />
+                      <LockClosedIcon className="h-5 w-5 text-green-300 group-hover:text-green-100" aria-hidden="true" />
                     </span>
                     Entrar
                   </button>
